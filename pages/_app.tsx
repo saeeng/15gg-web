bb43f63914532ae99d11644e8c81b09763f5040d
saeeng <saeeng.opt@gamil.com>
2021-08-05 22:22:10 +0900

    feat : init 프로젝트 초기 설정
    - tsx next-create-app --typescript
    - material ui 세팅 추가 , _document.tsx
    - prettier
    - tslint 초기 설

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react';
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
